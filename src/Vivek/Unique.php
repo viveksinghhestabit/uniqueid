716a9f6fd6ef214f53d25f3719aa9fc06ba20957
viveksinghhestabit <121616982+viveksinghhestabit@users.noreply.github.com>
2023-02-02 09:52:57 +0000

    m

--- src/Vivek/Unique.php
+++ src/Vivek/Unique.php
@@ -4,7 +4,7 @@ namespace Vivek;
 
 class UniqueId
 {
-    public function generate($length=19)
+    public static function generate($length=19)
     {
         $key = (int) (microtime(true) * 1000000000);
         $id = substr($key,0,$length);
