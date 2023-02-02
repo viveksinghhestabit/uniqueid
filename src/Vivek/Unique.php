66d10877be17b1b05dd2331b6f6362b520962306
viveksinghhestabit <121616982+viveksinghhestabit@users.noreply.github.com>
2023-02-02 07:28:49 +0000

    m

---
+++ src/Vivek/Unique.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Vivek;
+
+class UniqueId
+{
+    public function generate($length=19)
+    {
+        $key = (int) (microtime(true) * 1000000000);
+        $id = substr($key,0,$length);
+        return (int)$id;
+    }
+}
